ff5ded8d52de125e7c2b8263defb013ae0198d6c
Johnathan Garrett <johnathan.garrett@lmnsolutions.com>
2014-08-20 12:56:09 -0400

    Render pittsburgh buildings and strasbourg cathedral.

--- Apps/CesiumViewer/CesiumViewer.js
+++ Apps/CesiumViewer/CesiumViewer.js
@@ -1,10 +1,13 @@
 /*global define*/
+var viewer = null;
 define([
+        'Cesium',
         'Core/defined',
         'Core/formatError',
         'Core/getFilenameFromUri',
         'DataSources/CzmlDataSource',
         'DataSources/GeoJsonDataSource',
+        'DataSources/ConstantProperty',
         'Scene/TileMapServiceImageryProvider',
         'Widgets/Viewer/Viewer',
         'Widgets/Viewer/viewerCesiumInspectorMixin',
@@ -12,11 +15,13 @@ define([
         'Widgets/Viewer/viewerEntityMixin',
         'domReady!'
     ], function(
+        Cesium,
         defined,
         formatError,
         getFilenameFromUri,
         CzmlDataSource,
         GeoJsonDataSource,
+        ConstantProperty,
         TileMapServiceImageryProvider,
         Viewer,
         viewerCesiumInspectorMixin,
@@ -56,7 +61,6 @@ define([
         });
     }
 
-    var viewer;
     try {
         viewer = new Viewer('cesiumContainer', {
             imageryProvider : imageryProvider,
@@ -78,13 +82,71 @@ define([
     if (endUserOptions.inspector) {
         viewer.extend(viewerCesiumInspectorMixin);
     }
+    var buildingsURL = 'http://192.168.10.114/geoserver/geonode/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=geonode:cathederal_parts&propertyName=ht,min_ht,levels,min_levels&outputFormat=application/json';
+    var dataSource = new GeoJsonDataSource();
+    viewer.dataSources.add(dataSource);
+    dataSource.loadUrl(buildingsURL).then(function() {
+      console.log("loaded: ", dataSource.entities);
+      // Set material
+      if(dataSource.entities.entities.length > 0) {
+        dataSource.entities.entities[0].polygon.material.color.setValue(Cesium.Color.ANTIQUEWHITE);
+        dataSource.entities.entities[0].polygon.outline.setValue(false);
+      }
+      // Set heights
+      for (var idx = 0; idx < dataSource.entities.entities.length; idx++) {
+        var entity = dataSource.entities.entities[idx];
+        if(entity.properties.ht !== null && entity.properties.ht !== undefined) {
+          entity.polygon.extrudedHeight = new ConstantProperty(parseFloat(entity.properties.ht));
+        } else if (entity.properties.levels !== null && entity.properties.levels !== undefined) {
+          entity.polygon.extrudedHeight = new ConstantProperty(parseFloat(entity.properties.levels.replace(/\D/g, '')) * 3);
+        } else {
+          entity.polygon.extrudedHeight = new ConstantProperty(3);
+        }
+        if (entity.properties.min_ht !== null && entity.properties.min_ht !== undefined) {
+          entity.polygon.height = new ConstantProperty(parseFloat(entity.properties.min_ht));
+        } else if (entity.properties.min_levels !== null && entity.properties.min_levels !== undefined) {
+          entity.polygon.height = new ConstantProperty(parseFloat(entity.properties.min_levels.replace(/\D/g, '')) * 3);
+        } else {
+          entity.polygon.height = new ConstantProperty(0);
+        }
+      }
+    }, function(error) {
+      console.log("failed: ", error);
+    });
+
+    buildingsURL = 'buildings.json';
+    var dataSource2 = new GeoJsonDataSource();
+    viewer.dataSources.add(dataSource2);
+  dataSource2.loadUrl(buildingsURL).then(function() {
+      console.log("loaded: ", dataSource2.entities);
+      // Set material
+      if (dataSource2.entities.entities.length > 0) {
+        dataSource2.entities.entities[0].polygon.material.color.setValue(Cesium.Color.ANTIQUEWHITE);
+        dataSource2.entities.entities[0].polygon.outline.setValue(false);
+      }
+      // Set heights
+      for (var idx = 0; idx < dataSource2.entities.entities.length; idx++) {
+        var entity = dataSource2.entities.entities[idx];
+        if (entity.properties.height !== null && entity.properties.height !== undefined) {
+          entity.polygon.extrudedHeight = new ConstantProperty(parseFloat(entity.properties.height));
+        } else if (entity.properties.levels !== null && entity.properties.levels !== undefined) {
+          entity.polygon.extrudedHeight = new ConstantProperty(parseFloat(entity.properties.levels.replace(/\D/g, '')) * 3);
+        } else {
+          entity.polygon.extrudedHeight = new ConstantProperty(3 + Math.random()*75);
+        }
+        entity.polygon.height = new ConstantProperty(0);
+      }
+    }, function(error) {
+      console.log("failed: ", error);
+    });
 
     var layers = viewer.scene.imageryLayers;
     console.log('layers: ', layers);
+  console.log('dataSources', viewer.dataSources);
     //var wmsLayer =
-        layers.addImageryProvider(new Cesium.WebMapServiceImageryProvider({
-        url: 'http://geoserver2.rogue.lmnsolutions.com/geoserver/wms/geonode:incidentes_od3'
-    }));
+    //    layers.addImageryProvider(new Cesium.WebMapServiceImageryProvider({
+    //    url: 'http://geoserver2.rogue.lmnsolutions.com/geoserver/wms/geonode:incidentes_od3'
+    //}));
 
     var showLoadError = function(name, error) {
         var title = 'An error occurred while loading the file: ' + name;
